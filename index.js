81e2f5006518b9f208a01368a3d35a4011d179cf
tialantzy <tialantzy@gmail.com>
2022-07-01 11:52:42 -0500

    commit tree 6be84b8dad8fc6c802897590df8f6f482dc5947b

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31; 
+const num2 = 2; 
+const multiply = num1 * num2; 
+const random = Math.floor( Math.random ())* 10 + 1; 
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4; 
+const max = Math.max (4, 6, 8, 20);
 
-// Write your code here
+
+
+
